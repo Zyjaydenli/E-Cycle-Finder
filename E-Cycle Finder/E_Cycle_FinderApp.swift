1c21dffaee663a287ce5bbb997bc818e9b368800
Jayden <ziyanjayden.li@gmail.com>
2025-12-22 14:07:05 -0800

    Initial Commit

---
+++ E-Cycle Finder/E_Cycle_FinderApp.swift
@@ -0,0 +1,32 @@
+//
+//  E_Cycle_FinderApp.swift
+//  E-Cycle Finder
+//
+//  Created by Jayden Li on 2025-12-22.
+//
+
+import SwiftUI
+import SwiftData
+
+@main
+struct E_Cycle_FinderApp: App {
+    var sharedModelContainer: ModelContainer = {
+        let schema = Schema([
+            Item.self,
+        ])
+        let modelConfiguration = ModelConfiguration(schema: schema, isStoredInMemoryOnly: false)
+
+        do {
+            return try ModelContainer(for: schema, configurations: [modelConfiguration])
+        } catch {
+            fatalError("Could not create ModelContainer: \(error)")
+        }
+    }()
+
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+        .modelContainer(sharedModelContainer)
+    }
+}
